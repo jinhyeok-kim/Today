0bf52e122c0f6c7c7abe150ddd11dfc0d171197b
jinhyeok <persona5173@gmail.com>
2018-06-01 17:59:47 +0900

    Feat: Add DBHelper class

--- app/src/main/java/com/example/jinhyeok/whattoday/DBHelper.java
+++ app/src/main/java/com/example/jinhyeok/whattoday/DBHelper.java
@@ -1,16 +1,47 @@
 package com.example.jinhyeok.whattoday;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class DBHelper extends SQLiteOpenHelper {
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        
+
+    private Context context;
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        this.context = context;
     }
 
+    /**
+     * Database가 존재하지 않을 때, 딱 한번 실행되어 DB를 만드는 역할.
+     * @param db
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(" CREATE TABLE TEST_TABLE ( ");
+        sb.append(" _ID INTEGER PRIMARY KEY AUTOINCREMENT, ");
+        sb.append(" TIME TEXT, ");
+        sb.append(" CONTENTS TEXT ) ");
+
+        // SQLite Database로 쿼리 실행
+        db.execSQL(sb.toString());
+        Toast.makeText(context, "Table 생성완료", Toast.LENGTH_SHORT).show();
+
+    }
+
+    /**
+     * Application의 버전이 올라가서
+     * Table 구조가 변경되었을 때 실행된다.
+     *
+     * @param db
+     * @param oldVersion
+     * @param newVersion
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Toast.makeText(context, "버전이 올라갔습니다.", Toast.LENGTH_SHORT).show();
     }
 }
