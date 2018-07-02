df0f0f9774783854a46965714d205b133cfb5c49
jinhyeok <persona5173@gmail.com>
2018-07-02 16:31:10 +0900

    Feat: UI delete data import button

--- app/src/main/java/com/example/jinhyeok/whattoday/MainActivity.java
+++ app/src/main/java/com/example/jinhyeok/whattoday/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -42,12 +43,6 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.my_toolbar)
     Toolbar myToolbar;
 
-    //Layout Bind & Layout parameter Setting
-    @BindView(R.id.alarmLayout)
-    LinearLayout alarmLayout;
-    LinearLayout.LayoutParams paramText = new LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
-
     //Button Bind
     @BindView(R.id.startAlarm)
     Button startAlarmButton;
@@ -69,8 +64,6 @@ public class MainActivity extends AppCompatActivity {
     //DB View
     @BindView(R.id.lvWorks)
     ListView lvWork;
-    @BindView(R.id.btnSelectAllData)
-    Button btnSelectAllData;
 
 
     //Setting
@@ -78,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
     //HorizontalCalendar
     private HorizontalCalendar horizontalCalendar;
+    private String selectedDateStr;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -104,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
 
         dbHelper.testDB();
 
+
+
         /* start 2 months ago from now */
         Calendar startDate = Calendar.getInstance();
         startDate.add(Calendar.MONTH, -2);
@@ -115,6 +111,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Default Date set to Today.
         final Calendar defaultSelectedDate = Calendar.getInstance();
+
+        dayOfData(DateFormat.format("yyyy-MM-dd", defaultSelectedDate).toString())b;
 
         horizontalCalendar = new HorizontalCalendar.Builder(this, R.id.calendarView)
                 .range(startDate, endDate)
@@ -153,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDateSelected(Calendar date, int position) {
 //                selectedDateStr = DateFormat.format("EEE, MMM d, yyyy", date).toString();
-                String selectedDateStr = DateFormat.format("yyyy-MM-dd", date).toString();
+                selectedDateStr = DateFormat.format("yyyy-MM-dd", date).toString();
                 dayOfData(selectedDateStr);
                 Toast.makeText(MainActivity.this, selectedDateStr + " selected!", Toast.LENGTH_SHORT).show();
                 Log.i("onDateSelected", selectedDateStr + " - Position = " + position);
@@ -171,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         registerAlarm();
-        AlarmAvailabeCheck();
+//        AlarmAvailabeCheck();
 
     }
 
@@ -212,15 +210,16 @@ public class MainActivity extends AppCompatActivity {
         // startAlarm 실행 Debug용 Toast
         Toast.makeText(this, "알람 시작", Toast.LENGTH_SHORT).show();
         // Device를 깨운 후 시스템 시간 기준 1초 후 부터 alarmIntent 실행 , 50초 단위로 반복 실행
-//        mAlarmManger.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-//                SystemClock.elapsedRealtime() + 1000,
-//                settingalarmIntervalTime(), alarmIntent());
+        mAlarmManger.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 1000,
+                settingalarmIntervalTime(), alarmIntent());
 
         //realTime으로 수정 완료
-        mAlarmManger.setRepeating(AlarmManager.RTC_WAKEUP,
-                alarmTriggerTime(),
-                settingalarmIntervalTime(), alarmIntent());
+//        mAlarmManger.setRepeating(AlarmManager.RTC_WAKEUP,
+//                alarmTriggerTime(),
+//                settingalarmIntervalTime(), alarmIntent());
     }
+
 
     //알람을 실행하는 시간 설정 함수
     private long alarmTriggerTime() {
@@ -288,30 +287,6 @@ public class MainActivity extends AppCompatActivity {
         return mAlarmIntent;
     }
 
-    @OnClick(R.id.btnSelectAllData)
-    public void visibleData(){
-        lvWork.setVisibility(View.VISIBLE);
-        // DB Helper가 Null이면 초기화
-        if( dbHelper == null){
-            dbHelper = new DBHelper(MainActivity.this, //현재 화면의 제어권자
-                    dbName, //데이터베이스 이름
-                    null, //커서팩토리 - null 이면 표준 커서 사용
-                    1); //데이터베이스 버전
-        }
-
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String currentTime = formatter.format(System.currentTimeMillis());
-
-
-        List works = dbHelper.getDayWorkData(currentTime);
-
-        // 1. DayWork 데이터를 모두 가져온다.
-//        List works = dbHelper.getAllWorkData();
-
-        // 2. ListView에 DayWork 데이터를 모두 보여줌
-        lvWork.setAdapter(new WorkListAdapter(works, MainActivity.this));
-    }
-
     public int settingalarmIntervalTime(){
 
         int time = 0;
@@ -347,28 +322,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        AlarmAvailabeCheck();
-
-        lvWork.setVisibility(View.VISIBLE);
-        // DB Helper가 Null이면 초기화
-        if( dbHelper == null){
-            dbHelper = new DBHelper(MainActivity.this, //현재 화면의 제어권자
-                    dbName, //데이터베이스 이름
-                    null, //커서팩토리 - null 이면 표준 커서 사용
-                    1); //데이터베이스 버전
-        }
+//        AlarmAvailabeCheck();
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         String currentTime = formatter.format(System.currentTimeMillis());
 
+        if(currentTime.equals(selectedDateStr)) {
+            dayOfData(currentTime);
+        }
 
-        List works = dbHelper.getDayWorkData(currentTime);
-
-        // 1. DayWork 데이터를 모두 가져온다.
-//        List works = dbHelper.getAllWorkData();
-
-        // 2. ListView에 DayWork 데이터를 모두 보여줌
-        lvWork.setAdapter(new WorkListAdapter(works, MainActivity.this));
     }
 
     public void dayOfData(String date){
@@ -392,6 +354,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void AlarmAvailabeCheck(){
+        Toast.makeText(getApplicationContext(), "이용이 제한된 시간입니다.", Toast.LENGTH_LONG).show();
         SimpleDateFormat formatter = new SimpleDateFormat("HHmm");
         int timeFormat = Integer.parseInt(formatter.format(System.currentTimeMillis()));
 
